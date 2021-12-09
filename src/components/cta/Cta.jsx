c8c19351fa9c3842dd76df6948b73f1699de7eb5
Chandan Kumar Patnaik <chandanpatnaik81@gmail.com>
2021-12-10 03:32:36 +0530

    register code part is done

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import "./cta.css"
 const Cta = () => {
     return (
-        <div>
-            this is cta
+        <div className="gpt3__cta ">
+            <div className="gpt3__cta__content">
+                <p>Early Excess to Get Started</p>
+                <h3>Register today & start exploring the endless possibilities.</h3>
+            </div>
+            <div className="gpt3__cta__button">
+                <button type="button">Get Started</button>
+            </div>
         </div>
     )
 }
